1ce86b2de26fec73757a0cd86f72f81a2d1ad0f6
Dana Ismagulova <diko9716@outlook.com>
2016-01-23 10:34:05 +0500

    fff

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            ConsoleColor oldColor = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("I ");
+           
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("am sexy and i know it");
+
+            Console.ForegroundColor = oldColor;
+        }
+
+    }
+}
